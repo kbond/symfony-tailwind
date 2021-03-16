acb9b8e14cf808eddf055956e3d74a8899dc78d8
Kevin Bond <kevinbond@gmail.com>
2021-03-16 10:19:16 -0400

    add file type sample

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -40,6 +41,9 @@ class PostType extends AbstractType
                 'widget' => 'single_text'
             ])
             ->add('body')
+            ->add('image', FileType::class, [
+                'mapped' => false,
+            ])
             ->add('published')
         ;
     }
